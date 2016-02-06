7ac0f523509c0fa2fe6b78e6804c6ce365fe0798
mmarquezs <mmsa1994@gmail.com>
2016-02-06 12:19:39 +0100

    I messed up the package a little on pypi so i changed the version number a couple of times.

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 here = path.abspath(path.dirname(__file__))
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
     name='libgenapi',
-    version='1.0.4',
+    version='1.0.7',
     description='Library to search on Library genesis',
     long_description=long_description,
     url='https://github.com/mmarquezs/libgen-python-api/',
