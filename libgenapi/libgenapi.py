5ad4f07f58963c9239cad0efb9e790cd058ea8fc
mmarquezs <mmsa1994@gmail.com>
2016-02-15 10:01:01 +0100

    Simplified code and edited so it meets PEP8 convention

--- libgenapi/libgenapi.py
+++ libgenapi/libgenapi.py
@@ -23,108 +23,128 @@ class MirrorsNotResolvingError(Exception):
     """
     Error shown when none of the mirrors are resolving.
     """
-    pass 
+    pass
+
 class Libgenapi(object):
     """
     Main class representing the library
     """
-    def __init__(self,mirrors=None):
-        self.g=grab.Grab()
-        self.mirrors=mirrors
-    def SetMirrors(self, listMirrors):
-        self.mirrors=listMirrors
-    def search(self,searchTerm,numberResults=25):
+    def __init__(self, mirrors=None):
+        self.grabber = grab.Grab()
+        self.mirrors = mirrors
+        self.__selected_mirror = None
+    def set_mirrors(self, list_mirrors):
         """
-        TODO: 
+        Sets the mirrors of Libgen Genesis
+        """
+        self.mirrors = list_mirrors
+    def __choose_mirror(self):
+        if self.__selected_mirror is not None:
+            return
+        if self.mirrors is None:
+            raise MissingMirrorsError("There are no mirrors!")
+        last = len(self.mirrors)-1
+        for i, mirror in enumerate(self.mirrors):
+            try:
+                if sys.version_info[0] < 3:
+                    url = mirror
+                    self.grabber.go(url)
+                else:
+                    url = mirror
+                    self.grabber.go(url)
+                self.__selected_mirror = mirror
+                break
+            except grab.GrabError:
+                if i == last:
+                    raise MirrorsNotResolvingError("None of the mirrors are resolving, check" + \
+                                                   "if they are correct or you have connection!")
+    def __parse_books(self):
+        book = {"author":None, "series":None, "title":None, "edition":None, "isbn":None,
+                "publisher":None, "year":None, "pages":None, "language":None, "size":None,
+                "extension":None, "mirrors":None}
+        i = 0
+        d_keys = ["author", "series_title_edition_and_isbn", "publisher", "year", "pages",
+                  "language", "size", "extension", "mirrors"]
+        parse_result = []
+        for result in self.grabber.doc.select('//body/table[3]/tr[position()>1]/td[position()>1',
+                                              'and position()<=10]'):
+            # if len(search_result)>=number_results:
+            #     break
+            if i > 11:
+                parse_result += [book]
+                i = 0
+                book = {"author":None, "series":None, "title":None, "edition":None, "isbn":None,
+                        "publisher":None, "year":None, "pages":None, "language":None, "size":None,
+                        "extension":None, "mirrors":None}
+            elif d_keys[i] == "mirrors":            # Getting mirror links
+                book[d_keys[i]] = [x.text() for x in result.select("a/@href")]
+            elif d_keys[i] == "series_title_edition_and_isbn": # Getting title,isbn,series,edition.
+                try:
+                    # If there isn't an exception there is series,isbn or edition or all,
+                    # now we have to separate it...
+                    #
+                    # Cheking if there is any "green" text.
+                    # If there is it means there is the title and something else.
+                    # This makes an exception if there is nothing, which means no green text.
+                    green_text = result.select("a/font")
+                    book["title"] = result.select("a/text()").text()
+                    # A regex I found for isbn, not sure if perfect but better than mine.
+                    reg_isbn = re.compile(r"(ISBN[-]*(1[03])*[ ]*(: ){0,1})*",
+                                          "(([0-9Xx][- ]*){13}|([0-9Xx][- ]*){10})")
+                    reg_edition = re.compile(r'(\[[0-9] ed\.\])')
+                    for element in green_text:
+                        if reg_isbn.search(element.text()) != None:  # isbn found
+                            book["isbn"] = [reg_isbn.search(_).group(0)
+                                            for _ in element.text().split(",")
+                                            if reg_isbn.search(_) != None]
+                        elif reg_edition.search(element.text()) != None:  # edition found
+                            book["edition"] = element.text()
+                        else:   # Series found
+                            book["series"] = element.text()
+                except weblib.error.DataNotFound:  #Easy, there is just the title.
+                    book["title"] = result.text()  # Title found
+            else:
+                book[d_keys[i]] = result.text()
+                i += 1
+        return parse_result
+    def search(self, search_term, number_results=25):
+        """
+        TODO:
         Add documentation
-        DONE -> Search multiple pages untile the numberResults is meet or the end.
+        DONE -> Search multiple pages untile the number_results is meet or the end.
         Check for strange encodings, other langauges chinese,etc..
         Simplify,simplify,simply...For exemple the book dictionary
         should start with all keys with an empty string.
         Change the actual output to json?
         Make a example terminal app that uses it
-        Add parameters to the search apart from the searchTerm
+        STARTED -> Add parameters to the search apart from the search_term
         """
-        if self.mirrors==None:
-            raise MissingMirrorsError("There are no mirrors!")
-        last=len(self.mirrors)-1
-        if type(self.mirrors)!=type([]):
-            self.mirrors=[self.mirrors]
-            url=""
-            selectedMirror=""
-        for i,mirror in enumerate(self.mirrors):
-            try:
-                if sys.version_info[0]<3:
-                    url=mirror+"/search.php?"+urllib.urlencode({"req":searchTerm})
-                    self.g.go(url)
-                else:
-                    url=mirror+"/search.php?"+urllib.parse.urlencode({"req":searchTerm})
-                    self.g.go(url)
-                    selectedMirror=mirror
+        if isinstance(self.mirrors, str):
+            self.mirrors = [self.mirrors]
+        url = self.__selected_mirror+"/search.php?"+urllib.parse.urlencode({"req":search_term})
+        search_result = []
+        nbooks = re.search(r'([0-9]*) books',
+                           self.grabber.doc.select("/html/body/table[2]/tr/td[1]/font").text())
+        nbooks = int(nbooks.group(1))
+        pages_to_load = int(math.ceil(number_results/25.0)) # Pages needed to be loaded
+        # Check if the pages needed to be loaded are more than the pages available
+        if pages_to_load > int(math.ceil(nbooks/25.0)):
+            pages_to_load = int(math.ceil(nbooks/25.0))
+        for page in range(1, pages_to_load+1):
+            if len(search_result) > number_results:  # Check if we got all the results
                 break
-            except grab.GrabError as e:
-                if i==last:
-                    raise MirrorsNotResolvingError("None of the mirrors are resolving, check if they are correct or you have connection!")
-                pass
-            searchResult=[]
-            nbooks=int(re.search(r'([0-9]*) books',self.g.doc.select("/html/body/table[2]/tr/td[1]/font").text()).group(1))
-            pages=int(math.ceil(nbooks/25.0))
-            pagesToLoad=int(math.ceil(numberResults/25.0))
-        if pagesToLoad>pages:
-            pagesToLoad=pages
-            book={}
-            i=0
-            dKeys=["author","series","title","edition","isbn","publisher","year","pages","language","size","extension","mirrors"]
-        for page in range(1,pagesToLoad+1):
-            if len(searchResult)>numberResults:
-                break
-            if sys.version_info[0]<3:
-                url=selectedMirror+"/search.php?"+urllib.urlencode({"req":searchTerm,"page":page})
-                self.g.go(url)
+            url = ""
+            if sys.version_info[0] < 3:
+                url = self.__selected_mirror+"/search.php?"+ \
+                      urllib.urlencode({"req":search_term,
+                                        "page":page})
             else:
-                url=selectedMirror+"/search.php?"+urllib.parse.urlencode({"req":searchTerm,"page":page})
-                self.g.go(url)
-            for result in self.g.doc.select('//body/table[3]/tr[position()>1]/td[position()>1 and position()<=10]'):
-                if len(searchResult)>=numberResults:
-                    break
-                if dKeys[i]=="mirrors":            # Getting mirror links
-                    book[dKeys[i]]=[x.text() for x in result.select("a/@href")]
-                elif i==1:          # Getting title,isbn,series,edition
-                    try:            # Dammit... There is series,isbn or edition or all, now we have to separate it...
-                        greenText=result.select("a/font")  # Cheking if there is any "green" text. If there is it means there is the title and something else.
-                        book["title"]=result.select("a/text()").text()  # This makes and exception if there is nothing, which means no green text. 
-                        # regIsbn=re.compile(r'([A-z])')  # There isn't letters? Then is an ISBN,I guess?¿? This would fail if there is a series with only numbers..
-                        regIsbn=re.compile(r"(ISBN[-]*(1[03])*[ ]*(: ){0,1})*(([0-9Xx][- ]*){13}|([0-9Xx][- ]*){10})")  # A regex I found for isbn, not sure if perfect but better than mine. 
-                        regEdition=re.compile(r'(\[[0-9] ed\.\])')
-                        for element in greenText:
-                            if regIsbn.search(element.text())!=None:
-                                book["isbn"]=[]
-                                # book["isbn"]=[]
-                                for x in element.text().split(","):
-                                    if regIsbn.search(x)!=None:
-                                        book["isbn"]+=[regIsbn.search(x).group(0)]
-                                        # for isbn in regIsbn.seach(x):
-                                        #     book["isbn"]+=[isbn]
-                            elif regEdition.search(element.text())!=None:
-                                book["edition"]=element.text()
-                            else:
-                                book["series"]=element.text()
-                                i=i+3
-                    except weblib.error.DataNotFound:         #Easy, there is just the title. 
-                        book["series"]=""  # Series Empty
-                        i+=1
-                        book["title"]=result.text()  # Title
-                        i+=1
-                        book["edition"]=""  # Edition empty
-                        i+=1
-                        book["isbn"]="[]"  # ISBN empty
-                else:
-                    book[dKeys[i]]=result.text()
-                    i+=1
-                if i>11:
-                    searchResult+=[book]
-                    i=0
-                    book={}
-            if page != pagesToLoad:
-                time.sleep(random.randint(250,1000)/1000.0) # Random delay because if you ask a lot of pages,your ip might get blocked.
-        return searchResult
+                url = self.__selected_mirror+"/search.php?"+ \
+                      urllib.parse.urlencode({"req":search_term,
+                                              "page":page})
+            self.grabber.go(url)
+            search_result += self.__parse_books()
+            if page != pages_to_load:
+                # Random delay because if you ask a lot of pages,your ip might get blocked.
+                time.sleep(random.randint(250, 1000)/1000.0)
+            return search_result[:number_results]
