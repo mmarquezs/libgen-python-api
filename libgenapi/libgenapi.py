fe93545c88196b862d048bce72b25d460f626e41
mmarquezs <mmsa1994@gmail.com>
2018-04-07 19:46:11 +0200

    Fixed issue with some mirrors when getting the number of results.

--- libgenapi/libgenapi.py
+++ libgenapi/libgenapi.py
@@ -111,8 +111,9 @@ class Libgenapi(object):
                         urllib.parse.urlencode(request)
             g.go(url)
             search_result = []
-            nbooks = re.search(r'([0-9]*) books',
-                            g.doc.select("/html/body/table[2]/tr/td[1]/font").text())
+            nbooks = re.search(
+                r'([0-9]*) (books|files)',
+                g.doc.select("/html/body/table[2]/tr/td[1]/font").text())
             nbooks = int(nbooks.group(1))
             pages_to_load = int(math.ceil(number_results/25.0)) # Pages needed to be loaded
             # Check if the pages needed to be loaded are more than the pages available
